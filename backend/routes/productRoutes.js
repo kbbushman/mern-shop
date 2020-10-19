6e78a1c962e1ceddb8a479473bc74a3031b8536d
kbbushman <k.bushman@yahoo.com>
2020-10-19 01:41:52 -0700

    Adds product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from '../models/Product.js';
+
+const router = express.Router();
+
+
+// @desc    GET All Products
+// @route   GET /api/v1/products
+// @access  Public
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+}));
+
+
+// @desc    Get One Product
+// @route   GET /api/v1/products/:id
+// @access  Public
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({message: 'Product not found'});
+    }
+}));
+
+
+export default router;
