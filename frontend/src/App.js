3d7743b92eaba7870e365d748127f4638e5fb426
kbbushman <k.bushman@yahoo.com>
2020-10-18 18:10:43 -0700

    Adds routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
-    <div>
-      ProShop
-    </div>
+    <Router>
+      <Header />
+      <main className='py-5'>
+        <Container>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/product/:id' component={ProductPage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
