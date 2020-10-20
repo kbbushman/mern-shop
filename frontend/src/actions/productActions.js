f16568a6d316581e038b91cd43d17da2c1d649e8
kbbushman <k.bushman@yahoo.com>
2020-10-20 14:27:22 -0700

    Adds get product details action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,16 +3,35 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (id) => async (dispatch) => {
   try {
       dispatch({type: PRODUCT_LIST_REQUEST});
-      const { data } = await axios.get('/api/v1/products');
+      const { data } = await axios.get(`/api/v1/products/${id}`);
       dispatch({type:PRODUCT_LIST_SUCCESS, payload: data});
   } catch (error) {
     dispatch({
       type:PRODUCT_LIST_FAIL,
+      payload: error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
+
+
+export const listProductDetails = () => async (dispatch) => {
+  try {
+      dispatch({type: PRODUCT_DETAILS_REQUEST});
+      const { data } = await axios.get('/api/v1/products');
+      dispatch({type:PRODUCT_DETAILS_SUCCESS, payload: data});
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response && error.response.data.message
         ? error.response.data.message
         : error.message,
