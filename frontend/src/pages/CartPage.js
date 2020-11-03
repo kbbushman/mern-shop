8c520a940d48f097442cfadc29a93fc79ae84943
kbbushman <k.bushman@yahoo.com>
2020-11-03 13:16:47 -0800

    Adds removeFromCart dispatch

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartPage = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -17,7 +17,7 @@ const CartPage = ({ match, location, history }) => {
   const { cartItems } = cart;
 
   const removeFromCartHandler = (id) => {
-    console.log('Remove Item');
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
