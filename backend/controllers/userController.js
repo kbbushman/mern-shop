cc6eeb1a5ddaaa205d7a1076f5f603b9f66c1cf6
kbbushman <k.bushman@yahoo.com>
2020-11-03 16:09:11 -0800

    Adds user login

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
+import User from '../models/User.js';
+
+
+// @desc    Authenticate User & Issue Token
+// @route   GET /api/v1/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({email});
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email and/or password');
+  }
+});
+
+
+
+export {
+  authUser,
+};
