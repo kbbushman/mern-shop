32f30297aad21c94bdac1509630a0d68bd09eb08
kbbushman <k.bushman@yahoo.com>
2020-11-03 17:15:24 -0800

    Adds user model pre-save hook to hash new user password

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,9 +22,20 @@ const userSchema = new mongoose.Schema({
   },
 }, {timestamps: true});
 
+// Model Method - Compare Passwords
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Model Pre-Save Hook
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
