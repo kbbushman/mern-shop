ba6cd5251bb33c0a2d62c4c0a79f26f8b2c31d15
kbbushman <k.bushman@yahoo.com>
2020-11-04 23:56:12 -0800

    Fixes login route method

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 
 router.route('/').post(registerUser);
-router.route('/login').get(authUser);
+router.route('/login').post(authUser);
 router.route('/profile').get(protect, getUserProfile);
 
 
