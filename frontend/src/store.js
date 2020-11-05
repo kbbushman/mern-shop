b520db5492050539905b80b2c2ec924ae8ab0db6
kbbushman <k.bushman@yahoo.com>
2020-11-04 23:19:24 -0800

    Adds userLogin reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,21 +1,31 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
+import {
+  productListReducer,
+  productDetailsReducer
+} from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: {cartItems: cartItemsFromStorage},
+  userLogin: {userInfo: userInfoFromStorage},
 };
 
 const middleware = [thunk];
