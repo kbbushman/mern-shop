5737c4dec9b30e985054a0f6df4a64dcde3d1a8c
kbbushman <k.bushman@yahoo.com>
2020-11-04 23:20:08 -0800

    Adds user login

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+} from '../constants/userConstants';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+
+    const { data } = await axios.post('/api/v1/users/login', {email, password}, config);
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload: error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
