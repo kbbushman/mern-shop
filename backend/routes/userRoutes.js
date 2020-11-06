50c86ec06563a633637728d7fe4c598a503d99aa
kbbushman <k.bushman@yahoo.com>
2020-11-05 18:35:17 -0800

    Adds update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -11,7 +12,9 @@ const router = express.Router();
 
 router.route('/').post(registerUser);
 router.route('/login').post(authUser);
-router.route('/profile').get(protect, getUserProfile);
+router.route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 
 export default router;
