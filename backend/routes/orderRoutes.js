a1c3e5267b3e6f24279291d1f2c17182901caf20
kbbushman <k.bushman@yahoo.com>
2020-11-11 23:19:12 -0800

    Adds updateOrderToPaid

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 
 export default router;
