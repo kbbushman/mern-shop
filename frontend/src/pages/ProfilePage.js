fc6754ceed317287668f0b6e4e6752054a2244db
kbbushman <k.bushman@yahoo.com>
2020-11-12 02:13:43 -0800

    Adds user orders to view

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, Table } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
-import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
+// import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants';
+import { listUserOrders } from '../actions/orderActions';
 
-const ProfilePage = ({ history, location }) => {
+const ProfilePage = ({ history }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -16,18 +18,20 @@ const ProfilePage = ({ history, location }) => {
   const userDetails = useSelector((state) => state.userDetails);
   const userLogin = useSelector((state) => state.userLogin);
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const orderUserList = useSelector((state) => state.orderUserList);
   const { loading, error, user } = userDetails;
   const { userInfo } = userLogin;
   const { success } = userUpdateProfile;
+  const { loading:loadingOrders, error:errorOrders, orders } = orderUserList;
 
 
   useEffect(() => {
     if (!userInfo) {
       history.push('/login');
     } else {
-      if (!user || !user.name) {
-        dispatch({type: USER_UPDATE_PROFILE_RESET});
+      if (!user.name) {
         dispatch(getUserDetails('profile'));
+        dispatch(listUserOrders());
       } else {
         setName(user.name);
         setEmail(user.email);
@@ -94,6 +98,51 @@ const ProfilePage = ({ history, location }) => {
       </Col>
       <Col md={9}>
         <h2>My Orders</h2>
+        {loadingOrders
+          ? <Loader  />
+          : errorOrders
+          ? <Message variant='danger'>{errorOrders}</Message>
+          : (
+              <Table striped bordered hover responsive className='table-sm'>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>DATE</th>
+                    <th>TOTAL</th>
+                    <th>PAID</th>
+                    <th>DELIVERED</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order) => (
+                    <tr key={order._id}>
+                      <td>{order._id}</td>
+                      <td>{order.createdAt.substring(0, 10)}</td>
+                      <td>${order.totalPrice}</td>
+                      <td className='text-center'>
+                        {order.isPaid
+                          ? order.paidAt.substring(0, 10)
+                          : <i className="fas fa-times" style={{color: 'red'}}></i>
+                        }
+                      </td>
+                      <td  className='text-center'>
+                        {order.isDelivered
+                          ? order.DeliveredAt.substring(0, 10)
+                          : <i className="fas fa-times" style={{color: 'red'}}></i>
+                        }
+                      </td>
+                      <td>
+                        <LinkContainer to={`/order/${order._id}`}>
+                          <Button className='btn-sm' variant='light'>Details</Button>
+                        </LinkContainer>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )
+        }
       </Col>
     </Row>
   )
