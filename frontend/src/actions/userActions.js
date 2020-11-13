662e9db18715d91595726092c4c627cf436b61da
kbbushman <k.bushman@yahoo.com>
2020-11-13 00:44:06 -0800

    Adds user profile and order list reset dispatch at logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -10,10 +10,13 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants';
+import { ORDER_USER_LIST_RESET } from '../constants/orderConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -49,6 +52,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({type: USER_LOGOUT});
+  dispatch({type: USER_DETAILS_RESET});
+  dispatch({type: ORDER_USER_LIST_RESET});
 };
 
 
@@ -146,6 +151,17 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       type: USER_LOGIN_SUCCESS,
       payload: data,
     });
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+
+    setTimeout(() => {
+      dispatch({
+        type: USER_UPDATE_PROFILE_RESET,
+      });
+    }, 4000);
     
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
