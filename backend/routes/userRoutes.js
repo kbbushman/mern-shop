f5d1db7ae366a60a8e400ecb4650ec0af189a0fe
kbbushman <k.bushman@yahoo.com>
2020-11-15 00:00:11 -0800

    Adds delete user route for admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
+  deleteUser,
 } from '../controllers/userController.js';
 import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
@@ -14,10 +15,13 @@ const router = express.Router();
 router.route('/')
   .get(protect, adminOnly, getUsers)
   .post(registerUser);
-router.route('/login').post(authUser);
+router.route('/login')
+  .post(authUser);
 router.route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+router.route('/:id')
+  .delete(protect, adminOnly, deleteUser);
 
 
 export default router;
