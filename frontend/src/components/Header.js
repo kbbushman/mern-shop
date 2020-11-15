677a3320fec687ba3ba6a0d5c4d33ce972b4eadf
kbbushman <k.bushman@yahoo.com>
2020-11-14 23:34:46 -0800

    Adds resource links for admin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { logout } from '../actions/userActions.js';
 
 const Header = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
   const logoutHandler = () => {
     dispatch(logout());
+    history.push('/');
   };
 
   return (
@@ -37,10 +40,22 @@ const Header = () => {
                 </NavDropdown>
               ) : (
                 <LinkContainer to='/login'>
-                <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
-              </LinkContainer>
+                  <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
+                </LinkContainer>
               )}
-              
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                <LinkContainer to='/admin/userlist'>
+                  <NavDropdown.Item>Users</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/admin/productlist'>
+                  <NavDropdown.Item>Products</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/admin/orderlist'>
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
