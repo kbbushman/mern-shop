0e8154d36d0f894a0688beccf56309ed4b6f09a5
kbbushman <k.bushman@yahoo.com>
2020-11-15 00:40:21 -0800

    Adds getUserById and updateUser routes for admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,21 +6,28 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
 import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-
-router.route('/')
+router
+  .route('/')
   .get(protect, adminOnly, getUsers)
   .post(registerUser);
-router.route('/login')
+router
+  .route('/login')
   .post(authUser);
-router.route('/profile')
+router
+  .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route('/:id')
+router
+  .route('/:id')
+  .get(protect, adminOnly, getUserById)
+  .put(protect, adminOnly, updateUser)
   .delete(protect, adminOnly, deleteUser);
 
 
