2a7d6228d5b06212cf84920be3487391481de3e3
kbbushman <k.bushman@yahoo.com>
2020-11-16 00:22:40 -0800

    Adds delete product route for admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import {
   getProducts,
-  getProductById
+  getProductById,
+  deleteProduct,
 } from '../controllers/productController.js';
+import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(getProducts);
+  
 router
   .route('/:id')
-  .get(getProductById);
+  .get(getProductById)
+  .delete(protect, adminOnly, deleteProduct);
 
 
 export default router;
