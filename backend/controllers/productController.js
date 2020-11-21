8de3c7413f0b848ef0fc6cd9e8b907524aca2f89
kbbushman <k.bushman@yahoo.com>
2020-11-20 22:44:58 -0800

    Adds createProductReview for logged in user

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -93,10 +93,50 @@ const updateProduct = asyncHandler(async (req, res) => {
 });
 
 
+// @desc    Create New Review
+// @route   PUT /api/v1/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+  
+  if (product) {
+    const alreadyReviewed = product.reviews.find((review) => {
+      return review.user.toString() === req.user._id.toString();
+    });
+
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment: comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating = product.reviews.reduce((acc, review) => review.rating + acc, 0) / product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({message: 'Review added successfully'});
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
