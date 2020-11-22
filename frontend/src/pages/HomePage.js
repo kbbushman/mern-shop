7ea89306ebfed543d0387412ac8c2c3598f587f6
kbbushman <k.bushman@yahoo.com>
2020-11-21 23:05:53 -0800

    Adds back to all products button in search view

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Message from '../components/Message';
@@ -23,7 +24,10 @@ const HomePage = ({ match }) => {
   return (
     <>
       <Meta />
-      {!keyword && <ProductCarousel />}
+      {!keyword
+        ? <ProductCarousel />
+        : <Link to='/' className='btn btn-light'>Back to All Products</Link>
+      }
       <h1>Latest Products</h1>
       {loading
         ? <Loader />
