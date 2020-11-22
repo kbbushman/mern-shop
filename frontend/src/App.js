e740ba904c56ff3bffd1a45250bd002fc2032f1c
kbbushman <k.bushman@yahoo.com>
2020-11-21 21:04:09 -0800

    Adds admin product list pagination route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,8 @@ function App() {
           <Route path='/cart/:id?' component={CartPage} />
           <Route path='/admin/userlist' component={UserListPage} />
           <Route path='/admin/user/:id' component={UserEditPage} />
-          <Route path='/admin/productlist' component={ProductListPage} />
+          <Route path='/admin/productlist' component={ProductListPage} exact />
+          <Route path='/admin/productlist/:pageNumber' component={ProductListPage} exact />
           <Route path='/admin/product/:id/edit' component={ProductEditPage} />
           <Route path='/admin/orderlist' component={OrderListPage} />
           <Route path='/search/:keyword' component={HomePage} exact />
